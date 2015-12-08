d1d85f68dc3e584ba2dee6036872c6942a9a28b3
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-12-08 14:28:33 -0800

    Datascraping done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var secret = "snowflakes";
 var mongoose = require('mongoose');
 var Item = require('./models/item');
 var User = require('./models/user');
-mongoose.connect('mongodb://localhost/holiday-party');
+mongoose.connect('mongodb://localhost/holiday-party-items');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -28,7 +28,8 @@ app.use(function (err, req, res, next) {
 
 app.use('/api/items', require('./controllers/items'));
 app.use('/api/users', require('./controllers/users'));
-app.use('/api/test', require('./controllers/test'));
+app.use('/api/cookietest', require('./controllers/cookietest'));
+app.use('/api/drinktest', require('./controllers/drinktest'));
 
 app.post('/api/auth', function(req, res) {
  User.findOne({email: req.body.email}, function(err, user) {
